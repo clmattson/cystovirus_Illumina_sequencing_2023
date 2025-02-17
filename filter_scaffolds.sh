ea91a08d04b3f69bd33e302266633064c364d338
clmattson <114251551+clmattson@users.noreply.github.com>
2025-02-17 12:23:34 -0800

    Update filter_scaffolds.sh

--- filter_scaffolds.sh
+++ filter_scaffolds.sh
@@ -2,14 +2,16 @@
 
 #get input:
 
+#no paths used, make sure to run in the directory where all the assembly output follders are stored. phi6 refs have to be stored here too for blast
+
 #-p ${parent_dir} directory containing barcode folders
-#-d sample_suffix - starts with an underscore
+#-d sample_suffix - starts with an underscore, example: "_norm3_meta_spades"
 #-s ${host_s} S segment reference file & path
 #-m ${host_m} M segment reference file & path
 #-l ${host_l} L segment reference file & path
 
 
-parent_path=''
+#parent_path=''
 sample_suffix=''
 host_s=''
 host_m=''
@@ -20,10 +22,9 @@ print_usage() {
   printf "Usage: ..."
 }
 
-while getopts p:d:s:m:l: flag
+while getopts d:s:m:l: flag
 do
     case "${flag}" in
-p) parent_path=${OPTARG};;
 d) sample_suffix=${OPTARG};;
 s) host_s=${OPTARG};;
 m) host_m=${OPTARG};;
@@ -33,7 +34,7 @@ l) host_l=${OPTARG};;
 done
 
 
-cd ${parent_path} || exit
+#cd ${parent_path} || exit
 
 echo "working shell directory:"
 pwd
@@ -48,13 +49,13 @@ sample=${file##*/}
 sample=${file%%_*}; 
 #can add line here to remove contig header whitespace if necessary
 #blast the current assembly against phi6 M segment reference
-blastn -query ${sample}${sample_suffix}_output/${sample}${sample_suffix}_scaffolds.fasta -subject phi6_M.fasta -outfmt '6 delim=,' -qcov_hsp_perc 20 >  ${sample}${sample_suffix}_output/${sample}_norm3_meta_spades_scaffolds_M_blastout.csv;
-        blast_check="$(cat ${sample}${sample_suffix}_output/${sample}_norm3_meta_spades_scaffolds_M_blastout.csv)"
+blastn -query ${sample}${sample_suffix}_output/${sample}${sample_suffix}_scaffolds.fasta -subject phi6_M.fasta -outfmt '6 delim=,' -qcov_hsp_perc 20 >  ${sample}${sample_suffix}_output/${sample}${sample_suffix}_scaffolds_M_blastout.csv;
+        blast_check="$(cat ${sample}${sample_suffix}_output/${sample}${sample_suffix}_scaffolds_M_blastout.csv)"
         if [ -n "${blast_check}" ]; then
                 echo "${sample} ${blast_check} not empty"
                 #makes file with scaffold names only
-                awk -F"," '{print $1}' ${sample}${sample_suffix}_output/${sample}_norm3_meta_spades_scaffolds_M_blastout.csv > ${sample}${sample_suffix}_output/${sample}_norm3_meta_spades_scaffolds_M_blastout_names.txt;
-                cat ${sample}${sample_suffix}_output/${sample}_norm3_meta_spades_scaffolds_M_blastout_names.txt | awk '{gsub("_","\\_",$0);$0="(?s)^>"$0".*?(?=\\n(\\z|>))"}1' | pcregrep -oM -f - ${sample}${sample_suffix}_output/${sample}_norm3_meta_spades_scaffolds.fasta >> ${sample}${sample_suffix}_output/${sample}_norm3_meta_spades_M_scaffolds.fasta
+                awk -F"," '{print $1}' ${sample}${sample_suffix}_output/${sample}${sample_suffix}_scaffolds_M_blastout.csv > ${sample}${sample_suffix}_output/${sample}${sample_suffix}_scaffolds_M_blastout_names.txt;
+                cat ${sample}${sample_suffix}_output/${sample}${sample_suffix}_scaffolds_M_blastout_names.txt | awk '{gsub("_","\\_",$0);$0="(?s)^>"$0".*?(?=\\n(\\z|>))"}1' | pcregrep -oM -f - ${sample}${sample_suffix}_output/${sample}${sample_suffix}_scaffolds.fasta >> ${sample}${sample_suffix}_output/${sample}${sample_suffix}_M_scaffolds.fasta
         fi;
 done
 
@@ -114,8 +115,8 @@ for file in *${sample_suffix}_output/*${sample_suffix}_M_scaffolds.fasta;
     fi;
   done;
   #after going thru whole file, add the sequences baseed on the filtered headers to new fasta called ..covfilt...fasta
-  awk 'BEGIN{FS="\n";RS=">";ORS=""} (NR==FNR)&&(NR>1){headers[$1];next} ($1 in headers){print ">" $0}' *${sample_suffix}_output/${fileonly}_covlenfilt_names.txt *${sample_suffix}_output/${sample}_norm3_meta_spades_M_scaffolds.fasta >> *${sample_suffix}_output/${sample}_norm3_meta_spades_M_covlenfilt_scaffolds.fasta;
+  awk 'BEGIN{FS="\n";RS=">";ORS=""} (NR==FNR)&&(NR>1){headers[$1];next} ($1 in headers){print ">" $0}' *${sample_suffix}_output/${fileonly}_covlenfilt_names.txt *${sample_suffix}_output/${sample}${sample_suffix}_M_scaffolds.fasta >> *${sample_suffix}_output/${sample}${sample_suffix}_M_covlenfilt_scaffolds.fasta;
   echo "kcov values above 30% or 300 = ${filtered_cov_values[@]} + length values above 20% or 800 = ${filtered_len_values[@]}";
   echo;
-  echo "${sample},$(grep -c '>' *${sample_suffix}_output/${sample}_norm3_meta_spades_M_covlenfilt_scaffolds.fasta),${filtered_len_values[0]},${filtered_len_values[-1]},${filtered_cov_values[0]},${filtered_cov_values[-1]}" >> M_all_scaffold_stats.csv;
+  echo "${sample},$(grep -c '>' *${sample_suffix}_output/${sample}${sample_suffix}_M_covlenfilt_scaffolds.fasta),${filtered_len_values[0]},${filtered_len_values[-1]},${filtered_cov_values[0]},${filtered_cov_values[-1]}" >> M_all_scaffold_stats.csv;
 done
